7886997c1824f419c21041a424d56e9a9a17cc89
Zachary Siegel <siegelzc@gmail.com>
2024-05-09 11:40:43 -0500

    static template

--- src/wasmJsMain/kotlin/Main.kt
+++ src/wasmJsMain/kotlin/Main.kt
@@ -1,14 +1,2 @@
-import eval.pathspecInput
-import eval.testCaseInput
-import kotlinx.browser.document
-import kotlinx.html.dom.append
-import kotlinx.html.js.div
-
 fun main() {
-    document.body!!.append {
-        div(classes = "input-container") {
-            pathspecInput()
-            testCaseInput()
-        }
-    }
 }
